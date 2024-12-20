8fb6178b36f87a6af1d2ee4513a46dd073299427
omarbinsaleh <omarbinsaleh44@gmail.com>
2024-12-20 21:21:29 +0400

    crated an api for retrieving all jobs and an api for post a job

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.yolij.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fev0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,8 +22,12 @@ const client = new MongoClient(uri, {
   },
 })
 
+
 async function run() {
   try {
+    // create data base and collections:
+    const db = client.db('Job_Nexus');
+    const jobsCollection = db.collection('jobs'); // jobs collections
 
     // the default route:
     app.get('/', (req, res) => {
@@ -32,6 +37,19 @@ async function run() {
     // the hello routes:
     app.get('/hello', (req, res) => {
       res.send({message:"Hello World"});
+    });
+
+    // add job:
+    app.post('/jobs/add', async (req, res) => {
+      const data = req.body;
+      const result = await jobsCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // get all jobs
+    app.get('/jobs', async(req, res) => {
+      const data = await jobsCollection.find().toArray()
+      res.send(data);
     })
 
     // Send a ping to confirm a successful connection
